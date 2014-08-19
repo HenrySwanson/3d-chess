8ea2a2c5b6d7b91a328204bde83f8082c3987249
Henry Swanson <henryswanson94@gmail.com>
2014-08-19 03:30:13 -0500

    Created a semi-functional unit test for board.cpp. It depends on a text file though, and a disgustingly formatted one at that. Not gonna come up with a better solution at this hour though.

--- test/test_board.cpp
+++ test/test_board.cpp
@@ -1,10 +1,13 @@
 #include "../src/board.h"
 
+#include <cstdlib>
+#include <fstream>
 #include <iostream>
 #include <sstream>
 #include <string>
 
 using std::cout;
+using std::cerr;
 using std::endl;
 
 using std::list;
@@ -94,6 +97,27 @@ string toString(Move m)
     return ss.str();
 }
 
+PieceType getPieceTypeFromChar(char name, bool color)
+{
+    switch(name)
+    {
+      case 'P': return (color == WHITE ? W_PAWN : B_PAWN);
+      case 'N': return KNIGHT;
+      case 'G': return GRIFFIN;
+      case 'D': return DRAGON;
+      case 'U': return UNICORN;
+      case 'R': return ROOK;
+      case 'B': return BISHOP;
+      case 'M': return MACE;
+      case 'W': return WIZARD;
+      case 'A': return ARCHER;
+      case 'C': return CANNON;
+      case 'Q': return QUEEN;
+      case 'K': return KING;
+      default:  return NIL;
+    }
+}
+
 /**
  * Puts pieces on the board, and then generates all the moves that the first
  * piece can make. Then it converts the moves to strings, sorts them, and
@@ -101,10 +125,12 @@ string toString(Move m)
  * true, otherwise false.
  * 
  * Parameters:
+ *     name - the name of the test
  *     pieces, indices - pieces and their locations
  *     expected - the string of concatenated sorted moves we expect to get
  */
-bool testConfiguration(list<Piece> pieces, list<int> indices, string expected)
+bool testConfiguration(string name, list<Piece> pieces, list<int> indices,
+        string expected)
 {
     Board b;
 
@@ -122,18 +148,110 @@ bool testConfiguration(list<Piece> pieces, list<int> indices, string expected)
 
     string concatenated = "";
     for(list<string>::iterator it = strs.begin(); it != strs.end(); it++)
-        concatenated += *it;
+        concatenated += (*it + "\n");
 
-    return (concatenated == expected);
+    if(concatenated != expected)
+    {
+        cout << "----Test \"" << name << "\" failed!----" << endl;
+        cout << "Expected: " << endl;
+        cout << expected << endl;
+        cout << "Received: " << endl;
+        cout << concatenated << endl;
+        return false;
+    }
+
+    cout << "----Test \"" << name << "\" passed.----" << endl;
+    return true;
+}
+
+/**
+ * Reads a bizarrely formatted text file containing lists of pieces and moves.
+ * It parses them, and feeds the pieces and moves to testConfiguration. This is
+ * pretty janky, but it does the job. (And it's not production code, at least?)
+ */
+int loadConfigurationsAndTest()
+{
+    int errors = 0;
+    // TODO what's convention? run from root dir, or from bin dir?
+    std::ifstream file ("test/movements_and_expected_strings.txt");
+    if(file.is_open())
+    {
+        string line;
+        bool loadingPieces = true; // if not, loading strings
+
+        string test_name;
+        list<Piece> pieces;
+        list<int> indices;
+        string move_strs = "";
+
+        while(getline(file, line))
+        {
+            if(line[0] == '#' || line == "")
+                continue;
+
+            if(line[0] == '+')
+            {
+                loadingPieces = true;
+                test_name = line.substr(1);
+            }
+            else if(line[0] == '-')
+            {
+                loadingPieces = false;
+            }
+            else if(line[0] == '=')
+            {
+                errors += testConfiguration(test_name, pieces, indices,
+                        move_strs);
+                pieces.clear();
+                indices.clear();
+                move_strs = "";
+            }
+            else if(loadingPieces)
+            {
+                bool moved;
+                int x, y, z;
+                char name, color;
+
+                stringstream ss;
+		        ss << line;
+                ss >> color >> name >> x >> y >> z >> moved;
+
+                bool colorBool = color == 'W' ? WHITE : BLACK;
+                PieceType pt = getPieceTypeFromChar(name, colorBool);
+
+                Piece p = createPiece(pt, colorBool, moved);
+                int i = Board::mailbox(x,y,z);
+
+                pieces.push_back(p);
+                indices.push_back(i);
+            }
+            else
+            {
+		        move_strs += (line + "\n");
+            }
+        }
+    }
+    else
+    {
+        cerr << "Unable to open file, exiting!" << endl;
+        exit(-1);
+    }
+
+    return errors;
 }
 
 int main(void)
 {
-    cout << "----Begin test----" << endl;
+    cout << "====Begin tests====" << endl;
 
-    // TODO load data from text files and run them through testConfiguration
+    loadConfigurationsAndTest();
 
-    cout << "----End test----" << endl;
+    // TODO finish up movements_and_expected_strings.txt
+    // and change that dumb name
+    // also, figure out some more appropriate tests
+    // P.S. stop staying up past 3am, idiot
+
+    cout << "====End tests====" << endl;
 
     return 0;
 }
