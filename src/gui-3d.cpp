a6d57f475ce43c481d28577382eb9865f3fd161c
Henry Swanson <henryswanson94@gmail.com>
2014-09-07 02:03:12 -0500

    Changed initial size and position of the main window. Dragging it away from the terminal and resizing was annoying.

--- src/gui-3d.cpp
+++ src/gui-3d.cpp
@@ -1,14 +1,12 @@
 #include "gui-3d.h"
 
-Gui3D::Gui3D() : wxFrame(NULL, wxID_ANY, wxT("3D Chess"), wxDefaultPosition, wxDefaultSize)
+Gui3D::Gui3D() : wxFrame(NULL, wxID_ANY, wxT("3D Chess"), wxDefaultPosition, wxSize(500,500))
 {
     board = new Board();
     board->setup();
 
     // Note: Don't have to destruct child windows
     display_canvas = new DisplayCanvas(this, board);
-
-    Center();
 }
 
 Gui3D::~Gui3D()
