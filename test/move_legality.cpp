2ae80f7b033e870561dcb9cf4f17665de507ae88
Henry Swanson <henryswanson94@gmail.com>
2014-09-09 23:12:43 -0500

    Added move legality tests.

--- test/move_legality.cpp
+++ test/move_legality.cpp
@@ -2,4 +2,53 @@
 
 #include "unit_test.h"
 
-// TODO make some tests!
+TEST(MoveLegality, Pin)
+{
+    // All castling partners will be in their starting places
+    Piece wk (KING, WHITE);
+    Piece wr (ROOK, WHITE);
+    Piece bw (WIZARD, BLACK);
+
+    Board b;
+    b.putPiece(wk, mailbox(4,4,0));
+    b.putPiece(wr, mailbox(4,4,1));
+    b.putPiece(bw, mailbox(4,4,3));
+
+    Move m;
+    m = Move(WHITE, QUIET, mailbox(4,4,1), mailbox(4,0,1));
+    EXPECT_FALSE(b.isLegalMove(m));
+
+    m = Move(WHITE, QUIET, mailbox(4,4,1), mailbox(4,4,2));
+    EXPECT_TRUE(b.isLegalMove(m));
+
+    m = Move(WHITE, QUIET, mailbox(4,4,0), mailbox(4,3,0));
+    EXPECT_TRUE(b.isLegalMove(m));
+}
+
+TEST(MoveLegality, Block)
+{
+    // All castling partners will be in their starting places
+    Piece bk (KING, BLACK);
+    Piece bb (BISHOP, BLACK);
+    Piece wr (ROOK, WHITE);
+
+    Board b;
+    b.putPiece(bk, mailbox(4,4,7));
+    b.putPiece(bb, mailbox(4,5,6));
+    b.putPiece(wr, mailbox(4,4,3));
+
+    Move m;
+    m = Move(BLACK, QUIET, mailbox(4,5,6), mailbox(4,4,5));
+    EXPECT_TRUE(b.isLegalMove(m));
+
+    m = Move(BLACK, QUIET, mailbox(4,5,6), mailbox(4,6,7));
+    EXPECT_FALSE(b.isLegalMove(m));
+
+    m = Move(BLACK, QUIET, mailbox(4,4,7), mailbox(4,3,7));
+    EXPECT_TRUE(b.isLegalMove(m));
+
+    m = Move(BLACK, QUIET, mailbox(4,4,7), mailbox(4,4,6));
+    EXPECT_FALSE(b.isLegalMove(m));
+}
+
+// TODO castle tests
